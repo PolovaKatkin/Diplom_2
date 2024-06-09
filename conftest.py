5ebd15278eab3cd725623f5504537ae71a17d61b
PolovaKatkin <epolova@gmail.com>
2024-06-09 17:09:37 +0300

    Изменены фикстуры для удаления и логина пользователя, а также добавлена фикстура получения токена

--- conftest.py
+++ conftest.py
@@ -13,21 +13,26 @@ def create_user():
     yield data_payload, response
 
 
+@allure.step('Логинимся под пользователем и получаем токен.')
+@pytest.fixture(scope='function')
+def token(create_user):
+    data_payload = create_user[0]
+    login = MethodsUser.login_user(data_payload)
+    token = login.json()['accessToken']
+    return token, login
+
+
 @allure.step('Удаляем пользователя после теста.')
 @pytest.fixture(scope='function')
-def delete_user(create_user):
-    data_payload = create_user[0]
+def delete_user(token):
     yield
-    login = MethodsUser.login_user(data_payload)
-    token_user = login.json()['accessToken']
-    MethodsUser.delete_user(token_user)
+    MethodsUser.delete_user(token[0])
 
 
 @allure.step('Создаем пользователя, логинимся и удаляем после теста.')
 @pytest.fixture(scope='function')
-def login_user(create_user):
+def login_user(create_user, token, delete_user):
     data_payload = create_user[0]
-    login = MethodsUser.login_user(data_payload)
-    token_user = login.json()['accessToken']
+    login = token[1]
+    token_user = token[0]
     yield data_payload, login, token_user
-    MethodsUser.delete_user(token_user)
